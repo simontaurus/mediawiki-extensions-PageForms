f37f989fa4e3e781242a3cd5d68624ddfc7b28bc
Simon Stier <52674635+simontaurus@users.noreply.github.com>
2022-05-27 05:56:25 +0200

    Create PF_datetimeflatpicker.js

---
+++ libs/PF_datetimeflatpicker.js
@@ -0,0 +1,49 @@
+/**
+ * Javascript code to be used with input type datetimeflatpicker.
+ *
+ * @author Simon Stier
+ *
+ */
+
+/**
+ * Initializes a datetimeflatpicker input
+ * Test: https://jsfiddle.net/v8u4ts3q/
+ *
+ * @param {string} inputID the id of the input to initialize
+ * @param {Object} params the parameter object for the datetimeflatpicker, contains
+ *		currValue: (String) the current value (format: ISO time string)
+ *		minTime: (String) the minimum datatime to be shown (format: ISO time string)
+ *		maxTime: (String) the maximum datatime to be shown (format: ISO time string)
+ *		interval: (String) the interval between selectable times in minutes
+ *		format: (String) display format string 
+ */
+window.PF_DTFP_init = function( inputID, params ) { // minTime, maxTime, interval, format
+
+	var value = new Date();
+	if (params.currValue !== "") value = new Date(params.currValue);
+						      				      
+	var minDate = "";
+	if (params.minTime !== "") minDate = new Date(params.minTime);
+	var maxDate = "";
+	if (params.maxTime !== "") maxDate = new Date(params.maxTime);
+	
+	var format = params.format;
+	if (format === "") format = "Y-m-d H:i";
+	
+	var increment = params.interval;
+	if (increment === "") increment = 5;
+
+	flatpickr("#" + inputID, {
+			defaultDate: value,
+			enableTime: true,
+			dateFormat: format,
+		  	minDate: minDate,
+  			manDate: maxDate,
+  			minuteIncrement: increment,
+			onChange: function(selectedDates) {
+				var selectedDate = new Date(selectedDates[0]).toISOString();
+				console.log(selectedDate);
+	  	}
+	});
+
+};
